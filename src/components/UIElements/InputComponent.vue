033842a35976ed07145497b403b822512a2e1390
nathanlerit1991 <nathan.lerit@gmail.com>
2024-05-09 09:57:15 +0800

    update

--- src/components/UIElements/InputComponent.vue
+++ src/components/UIElements/InputComponent.vue
@@ -1,27 +1,55 @@
 <script setup>
+    import { ref, defineProps, defineEmits } from 'vue'
     //VUE PRIME 
     import InputText from 'primevue/inputtext'
 
     const props_data = defineProps({
         additional_class: {
             type: String,
-            default: () => ""
+            default: ''
         },
         id_name: {
             type: String,
-            default: () => ""
+            default: ''
         },
         type: {
             type: String,
-            default: () => ""
+            default: ''
         },
+        modelValue: {
+            type: String,
+            default: ''
+        }
     })
+
+    const emit = defineEmits(['update:modelValue', 'input'])
+    let val = ref(props_data.modelValue || '')
+    const handleInput = (event) => {
+        const value = event.target.value; // Extract value from InputEvent object
+        val.value = value
+        emit('update:modelValue', value)
+        emit('input', value) // Emit a custom input event with the value
+    }
+
+    let isPasswordVisible = ref(false)
 </script>
 
 <template>
-    <InputText
-        :type="props_data.type" 
-        :id="props_data.id_name ? props_data.id_name : null"
-        :class="`py-2 px-4 ${props_data.additional_class} ${props_data.type === 'password' ? 'password' : ''}`"
-    />
+    <div class="relative">
+        <InputText
+            v-model="val"
+            @input="handleInput"
+            :type="isPasswordVisible ? 'text' : props_data.type" 
+            :id="props_data.id_name ? props_data.id_name : null"
+            :class="
+                `py-2 px-4 ${props_data.additional_class} 
+                ${props_data.type === 'password' ? 'password pr-10' : ''}`
+            "
+        />
+        <i
+            v-if="props_data.type === 'password'" 
+            :class="isPasswordVisible ? 'pi pi-eye' : 'pi pi-eye-slash'"
+            @click="isPasswordVisible = !isPasswordVisible"
+        />
+    </div>
 </template>
