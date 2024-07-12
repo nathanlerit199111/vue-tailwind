da09e972582314c794e4b963b9d4a4a8f07ec1dc
Jerico Contreras <jerico@thinkbitsolutions.com>
2024-07-12 19:49:54 +0800

    feat: create props struct function for svg icons common props

---
+++ src/helpers/props-struct.js
@@ -0,0 +1,32 @@
+// createPropsStruct can be used like a defineProps
+// but, you can only override the default property
+export function createPropsStruct(definedProps = {}) {
+  const getDefault = (key, defaultPropsValue) => {
+    const definedDefault = definedProps?.[key]?.default || null
+    return typeof definedDefault === 'function'
+      ? definedDefault(args)
+      : definedDefault || defaultPropsValue
+  }
+
+  const defaultColor = 'red'
+  const defaultSizeInPixel = 25
+
+  return {
+    stroke: {
+      type: String,
+      default: getDefault('stroke', defaultColor)
+    },
+    fill: {
+      type: String,
+      default: getDefault('fill', defaultColor)
+    },
+    width: {
+      type: [String, Number],
+      default: getDefault('width', defaultSizeInPixel)
+    },
+    height: {
+      type: [String, Number],
+      default: getDefault('height', defaultSizeInPixel)
+    }
+  }
+}
