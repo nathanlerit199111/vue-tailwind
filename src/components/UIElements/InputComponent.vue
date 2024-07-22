b70579838c8eaa690d995d2283b69082a57683d9
Nathan <nathan@thinkbitsolutions.com>
2024-07-22 21:03:50 +0800

    simplify input component

--- src/components/UIElements/InputComponent.vue
+++ src/components/UIElements/InputComponent.vue
@@ -1,8 +1,10 @@
 <script setup>
-  import { ref, defineEmits, watch } from 'vue'
-  import { Field, ErrorMessage } from 'vee-validate';
+  import { ref } from 'vue'
+  import { Field, ErrorMessage } from 'vee-validate'
 
-  const props_data = defineProps({
+  const model = defineModel()
+
+  const props = defineProps({
     additional_class: {
       type: String,
       default: ''
@@ -15,9 +17,13 @@
       type: String,
       default: ''
     },
-    modelValue: {
-      type: [String, Number],
-      default: ''
+    minValue: {
+      type: Number,
+      default: null
+    },
+    maxValue: {
+      type: Number,
+      default: null
     },
     rules: {
       type: Function,
@@ -29,47 +35,40 @@
     }
   })
 
-  const emit = defineEmits(['update:modelValue'])
-  let internalValue = ref(props_data.modelValue)
-
-  const handleChange = (event) => {
-    const newValue = props_data.inputChange(event.target.value) // Apply inputChange
-    internalValue.value = newValue // Update internalValue with validated value
-    emit('update:modelValue', internalValue.value) // Emit update to parent component
-  }
-
-  // Watch for changes in props.modelValue and update internalValue accordingly
-  watch(() => props_data.modelValue, (newValue) => {
-    internalValue.value = newValue
-  })
-
   let isPasswordVisible = ref(false)
+
+  const handleInput = (event) => {
+    const inputValue = event.target.value;
+    const validatedValue = props.inputChange(inputValue);
+    model.value = validatedValue;
+  }
 </script>
 
 <template>
   <div class="relative h-max">
     <Field
       v-bind="$attrs"
-      v-model="internalValue"
-      :name="props_data.name" 
-      :type="isPasswordVisible ? 'text' : props_data.type"
-      :rules="props_data.rules"
-      @input="handleChange"
-      :class="
-        `py-2 px-4 ${props_data.additional_class} 
-        ${props_data.type === 'password' ? 'password pr-10' : ''}
-        ${props_data.type === 'search' ? 'search pr-10' : ''}
+      v-model="model"
+      @input="handleInput"
+      :name="props.name"
+      :type="isPasswordVisible ? 'text' : props.type"
+      :rules="props.rules"
+      :max="props.maxValue"
+      :min="props.minValue"
+      :class="`py-2 px-4 ${props.additional_class} 
+        ${props.type === 'password' ? 'password pr-10' : ''}
+        ${props.type === 'search' ? 'search pr-10' : ''}
         `"
     />
     <i
-      v-if="props_data.type === 'password'" 
+      v-if="props.type === 'password'"
       :class="isPasswordVisible ? 'pi pi-eye' : 'pi pi-eye-slash'"
       @click="isPasswordVisible = !isPasswordVisible"
     />
-    <i 
-      v-if="props_data.type === 'search'" 
-      class="pi pi-search" 
+    <i
+      v-if="props.type === 'search'"
+      class="pi pi-search"
     />
   </div>
-  <ErrorMessage :name="props_data.name" />
+  <ErrorMessage :name="props.name" />
 </template>
